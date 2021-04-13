75d8a97485342100436a5ae019d81ccba609faeb
JoeAmedeo <joseph_amedeo@yahoo.com>
2021-04-12 23:47:19 -0400

    working on parameterizing everything for the bash script

--- scripts/buildAndPublish.sh
+++ scripts/buildAndPublish.sh
@@ -1,17 +1,34 @@
 #!/usr/bin/env bash
 
-Version="0.0.1"
-Publish=false
+variable_exists () {
+    if ! [ -v $0 ]
+    then
+        echo "Missing variable: " + $1
+        exit 1
+    fi
+}
 
 for i in "$@"
 do
 case $i in
     -v=*|--version=*
-    Version="${i#*=}"
+    version="${i#*=}"
     shift
     ;;
     -p|--publish
     publish=true
+    shift
+    ;;
+    -u=*|--user=*
+    user="${i#*=}"
+    shift
+    ;;
+    -p=*|--password=*
+    password="${i#*=}"
+    shift
+    ;;
+    -i=*|--image=*
+    image="${i#*=}"
     shift
     ;;
     *)
@@ -20,10 +37,20 @@ case $i in
 esac
 done
 
-docker login -u ${Username} -p ${Password}
-docker build -t ${Username}/${Image}:${Version} -t ${Username}/${Image}:latest .
-if [ $Publish ]
+variable_exists user "user"
+variable_exists password "password"
+variable_exists image "image"
+
+if ! [ -v version] && [ -v publish ]
 then
-    docker push ${Username}/${Image}:${Version}
+    echo "A version number must be specified in order to publish a package"
+    exit 1
+fi
+
+docker login -u ${Username} -p ${Password}
+docker build -t ${Username}/${Image}:${version} -t ${Username}/${Image}:latest .
+if [ $publish = true ]
+then
+    docker push ${Username}/${Image}:${version}
     docker push ${Username}/${Image}:latest
 fi

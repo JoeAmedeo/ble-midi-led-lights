23d03208d004519239f4d51ec3ca128e11de344c
JoeAmedeo <joseph_amedeo@yahoo.com>
2021-04-12 23:26:04 -0400

    updated bash script to take version and publish args

--- scripts/buildAndPublish.sh
+++ scripts/buildAndPublish.sh
@@ -1,8 +1,29 @@
 #!/usr/bin/env bash
 
-Version=${GITHUB_REF#refs/*/}
+Version="0.0.1"
+Publish=false
+
+for i in "$@"
+do
+case $i in
+    -v=*|--version=*
+    Version="${i#*=}"
+    shift
+    ;;
+    -p|--publish
+    publish=true
+    shift
+    ;;
+    *)
+
+    ;;
+esac
+done
 
 docker login -u ${Username} -p ${Password}
 docker build -t ${Username}/${Image}:${Version} -t ${Username}/${Image}:latest .
-docker push ${Username}/${Image}:${Version}
-docker push ${Username}/${Image}:latest
+if [ $Publish ]
+then
+    docker push ${Username}/${Image}:${Version}
+    docker push ${Username}/${Image}:latest
+fi

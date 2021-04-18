756f8cdfbf08d8178f6dc65a7ee253bd06d27d64
JoeAmedeo <joseph_amedeo@yahoo.com>
2021-04-18 11:10:35 -0400

    removing the builder made it fail earlier

--- scripts/buildAndPublish.sh
+++ scripts/buildAndPublish.sh
@@ -66,6 +66,7 @@ buildVersionLatest=$( echo "-t $user/$image:latest" )
 buildTarget=$(! [ -v $target ] && echo "--target $target" || echo "")
 
 docker login -u $user -p $PASSWORD
+docker buildx create --use --name mybuilder
 docker buildx build --platform linux/arm64 $buildTarget $buildVersion $buildVersionLatest .
 if [ "$publish" = true ]
 then

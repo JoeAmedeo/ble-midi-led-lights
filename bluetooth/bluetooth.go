9592c5d601822777a022b1391b222c897783eceb
JoeAmedeo <joseph_amedeo@yahoo.com>
2021-05-05 21:37:19 -0400

    attempt the get device by address way

--- bluetooth/bluetooth.go
+++ bluetooth/bluetooth.go
@@ -5,8 +5,6 @@ import (
 
 	"github.com/muka/go-bluetooth/api"
 	"github.com/muka/go-bluetooth/bluez/profile/adapter"
-	"github.com/muka/go-bluetooth/bluez/profile/agent"
-	"github.com/muka/go-bluetooth/bluez/profile/device"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -23,68 +21,30 @@ func Run(macAddress string) error {
 		return err
 	}
 
-	adapterId, err := a.GetAdapterID()
+	mydevice, err := a.GetDeviceByAddress(macAddress)
 	if err != nil {
-		return err
+		return fmt.Errorf("obtaining device failed: %s", err)
 	}
-
-	log.Infof("Adapter created: %s", a.Properties.Name)
 	// don't know yet if I'll have to clear the bluez cache, if needed use a.FlushDevices()
 
 	// Have to make sure it's paired first
 
-	discovery, cancel, err := api.Discover(a, nil)
+	log.Infof("device address: %s", mydevice.Properties.Address)
+	log.Infof("device name: %s", mydevice.Properties.Name)
 
-	if err != nil {
-		return fmt.Errorf("discovery failed: %s", err)
-	}
-
-	defer cancel()
-
-	found := false
-
-	for discoveredDevice := range discovery {
-
-		mydevice, err := device.NewDevice1(discoveredDevice.Path)
-
-		log.Infof("device path: %s", discoveredDevice.Path)
-		log.Infof("device address: %s", mydevice.Properties.Address)
-		log.Infof("device name: %s", mydevice.Properties.Name)
-
-		if err != nil {
-			return fmt.Errorf("creating device failed: %s", err)
-		}
-
-		if mydevice.Properties.Address != macAddress {
-			continue
-		}
-
-		if mydevice.Properties.Paired {
-			continue
-		}
-
-		found = true
-		// log.Info(i, v.Path)
+	if !mydevice.Properties.Paired {
 		log.Infof("Pairing with %s", mydevice.Properties.Address)
-
 		err = mydevice.Pair()
-
 		if err != nil {
 			return fmt.Errorf("pair failed: %s", err)
 		}
+	}
 
-		log.Info("Pair succeed, connecting...")
-
-		agent.SetTrusted(adapterId, mydevice.Path())
-
+	if !mydevice.Properties.Connected {
 		err = mydevice.Connect()
 		if err != nil {
 			return fmt.Errorf("connect failed: %s", err)
 		}
-	}
-
-	if !found {
-		return fmt.Errorf("device not found")
 	}
 
 	log.Infoln("Connection succeeded!")
